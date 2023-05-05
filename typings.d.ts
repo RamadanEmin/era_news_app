b8577759cd353c1af1c2eb9e2dd2ef958968d4b5
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-05-05 12:03:51 +0300

    Added Typings.d

---
+++ typings.d.ts
@@ -0,0 +1,26 @@
+type Article = {
+    category: string | null;
+    country: string;
+    description: string;
+    image: string | null;
+    language: string;
+    published_at: string;
+    source: string;
+    title: string;
+    url: string;
+    author: string | null;
+};
+
+type Pagination = {
+    count: Int;
+    limit: Int;
+    offset: Int;
+    total: Int;
+};
+
+type NewsResponse = {
+    pagination: Pagination;
+    data: Article[];
+};
+
+type Category = 'general' | 'business' | 'entertainment' | 'health' | 'science' | 'sports' | 'technology';
