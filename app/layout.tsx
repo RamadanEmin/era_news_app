eb855e28179a20d5a12ceff15e6f55a507256952
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-05-06 11:59:49 +0300

    Added "NewsList" component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,26 @@
-import React from "react";
-import "./globals.css";
-import Header from "../components/Header";
+import React from 'react';
+import './globals.css';
+import { Metadata } from 'next';
+import Header from '../components/Header';
 
-export const metadata = {
-  title: "ERA_news",
-  description: "Generated ERA_news_app by create next app",
+export const metadata: Metadata = {
+    title: 'ERA_news',
+    description: 'Generated ERA_news_app by create next app',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className="bg-gray-100 dark:bg-zinc-900 transition-all duration-700">
-        <Header />
-        <div className="max-w-6xl mx-auto">{children}</div>
-      </body>
-    </html>
-  );
+    return (
+        <html>
+            <body className='bg-gray-100 dark:bg-zinc-900 transition-all duration-700'>
+                    < Header />
+                    <div className='max-w-6xl mx-auto'>
+                        {children}
+                    </div>
+            </body>
+        </html >
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { categories } from '@/constants';
+import fetchNews from '../lib/fetchNews';
+import NewsList from '@/components/NewsList';
 
 async function Home() {
+    const news: NewsResponse = await fetchNews(categories.join(','));
 
     return (
-        
+        <div>
+            <NewsList news={news} />
+        </div>
     );
 }
 
