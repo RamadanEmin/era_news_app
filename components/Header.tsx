2a6315fe5a77bad3928fce01bb23c9dcd960b199
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-05-08 09:22:10 +0300

    Added "Dark and Light mode" theme

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import { Bars3Icon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
 import NavLinks from './NavLinks';
 import SearchBox from './SearchBox';
+import DarkModeButton from './DarkModeButton';
 
 const Header = () => {
     return (
@@ -16,6 +17,7 @@ const Header = () => {
                     </h1>
                 </Link>
                 <div className='flex items-center justify-end space-x-2'>
+                    <DarkModeButton />
                     <button className='hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800'>
                         Subscribe Now
                     </button>
